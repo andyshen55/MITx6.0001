8998f847b57b54c987ae265ebc4dbce2c671fa73
Andy Shen <andyshen55@ucla.edu>
2019-08-28 07:16:39 +0630

    reformated ps1a, implemented ps1b

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -7,12 +7,16 @@ r = 0.04
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 #continues adding months until savings exceed down payment
 months = 0
 while (current_savings < total_cost * portion_down_payment):
-    current_savings += current_savings * (r / 12)
-    current_savings += annual_salary / 12 * portion_saved
     months += 1
-
+    current_savings *= 1 + (r / 12)
+    current_savings += annual_salary / 12 * portion_saved
+     #increases salary semi-annually after every 6th month
+    if not (months % 6):
+        annual_salary *= 1 + semi_annual_raise
+    
 print("Number of months:", str(months))
