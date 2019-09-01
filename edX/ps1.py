05a2cccf6b0e9dbd9bd2c6aa8ad3b830022cedcf
Andy Shen <andyshen55@ucla.edu>
2019-09-02 02:59:43 +0630

    cleaned up solutions into separate functions

--- edX/ps1.py
+++ edX/ps1.py
@@ -1,33 +1,41 @@
-s = 'azcbobobegghakl'
-# count = 0
-# for char in s:
-#     if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
-#         count += 1
-# print("Number of vowels:", str(count))
+def prob1(s):
+    count = 0
+    for char in s:
+        if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+            count += 1
+    print("Number of vowels:", str(count))
 
-# total = 0
-# for char in range(len(s) - 2):
-#     if s[char:char + 3] == 'bob':
-#         total += 1
-# print("Number of times bob occurs is:", total)
+def prob2(s):
+    total = 0
+    for char in range(len(s) - 2):
+        if s[char:char + 3] == 'bob':
+            total += 1
+    print("Number of times bob occurs is:", total)
 
-longest = 0
-current = 1
-currentIndex = 0
-longestIndex = 0
-for char in range(len(s) - 1):
-        curr = s[char]
-        nxt = s[char + 1]
-        if curr <= nxt:
-                current += 1
-        else:
-                if current > longest:
-                        longest = current
-                        longestIndex = currentIndex
-                current = 1
-                currentIndex = char + 1
+def prob3(s):
+    longest = 0
+    current = 1
+    currentIndex = 0
+    longestIndex = 0
+    for char in range(len(s) - 1):
+            curr = s[char]
+            nxt = s[char + 1]
+            if curr <= nxt:
+                    current += 1
+            else:
+                    if current > longest:
+                            longest = current
+                            longestIndex = currentIndex
+                    current = 1
+                    currentIndex = char + 1
 
-if current > longest:
-        longest = current
+    if current > longest:
+            longest = current
 
-print("Longest substring in alphabetical order is:", s[longestIndex: longestIndex + longest])
+    print("Longest substring in alphabetical order is:", s[longestIndex: longestIndex + longest])
+
+if __name__ == "__main__":
+    s = 'azcbobobegghakl'
+    prob1(s)
+    prob2(s)
+    prob3(s)
