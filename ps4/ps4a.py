15586452e2b95542ede0c41c5c9669af98477036
Andy Shen <andyshen55@ucla.edu>
2019-09-05 20:19:17 +0630

    implemented and tested ps4a

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -22,8 +22,16 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    results = []
+    #base case is a one char string
+    if len(sequence) <= 1:
+        return sequence
 
-    pass #delete this line and replace with your code here
+    #expected permutations is n!, so for every n-depth position, recurse with size n-1    
+    for letter in sequence:
+        #for every letter, reduce the problem by recursing on the sequence with the letter removed
+        results += [letter + perm for perm in get_permutations(sequence.replace(letter, ''))]
+    return results
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -36,5 +44,23 @@ if __name__ == '__main__':
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    #test1 
+    test_input1 = 'abc'
+    print('Input:', test_input1)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(test_input1))
+    print(len(get_permutations(test_input1)))
 
+    #test2
+    test_input2 = 'BUST'
+    print('Input:', test_input2)
+    print('Expected Output:', "BUST UBST SBUT BSUT USBT SUBT TUBS UTBS BTUS TBUS UBTS BUTS BSTU SBTU TBSU BTSU STBU TSBU TSUB STUB UTSB TUSB SUTB USTB".split())
+    print('Actual Output:', get_permutations(test_input2))
+    print(len(get_permutations(test_input2)))
+
+    #test3 
+    test_input3 = 'xy'
+    print('Input:', test_input1)
+    print('Expected Output:', ['xy', 'yx'])
+    print('Actual Output:', get_permutations(test_input3))
+    print(len(get_permutations(test_input3)))
