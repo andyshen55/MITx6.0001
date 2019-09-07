e7e2705d88afba2143a209d49b97708ec374bfb9
Andy Shen <andyshen55@ucla.edu>
2019-09-07 23:10:54 +0630

    implemented NewsStory class, passed test cases

--- ps5/ps5.py
+++ ps5/ps5.py
@@ -1,6 +1,6 @@
 # 6.0001/6.00 Problem Set 5 - RSS Feed Filter
-# Name:
-# Collaborators:
+# Name: Andy Shen
+# Collaborators: N/A
 # Time:
 
 import feedparser
@@ -55,7 +55,29 @@ def process(url):
 # Problem 1
 
 # TODO: NewsStory
+class NewsStory():
+    def __init__(self, guid, title, description, link, pubdate):
+        self.guid = guid
+        self.title = title
+        self.description = description
+        self.link = link
+        self.pubdate = pubdate
 
+    def get_guid(self):
+        return self.guid
+
+    def get_title(self):
+        return self.title
+
+    def get_description(self):
+        return self.description
+
+    def get_link(self):
+        return self.link
+
+    def get_pubdate(self):
+        return self.pubdate
+    
 
 #======================
 # Triggers
